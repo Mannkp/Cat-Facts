0bc20b9b96a9d067c4c50c46c780aa1f0286303b
Mannkumar_K._Pandya <75202120+Mannkp@users.noreply.github.com>
2023-06-02 16:39:29 +0530

    Added Html, Css, Js files and background image

---
+++ script.js
@@ -0,0 +1,17 @@
+const factContainer = document.querySelector(".fact-container");
+const factBtn = document.querySelector("#btn");
+
+const loadCatFact = async () => {
+    try{    
+        const res = await fetch("https://catfact.ninja/fact");
+        let data = await res.json();
+        console.log(data.fact);
+        // factBtn.innerText = "mkp";
+        factContainer.innerText = data.fact;
+    }
+    catch (e){
+        console.log("Error: " , e);
+    }
+}
+;
+factBtn.addEventListener('click' , loadCatFact);
